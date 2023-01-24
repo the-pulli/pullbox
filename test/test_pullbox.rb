e4794edc48d9eeba55b066e2535ed5ce7c2d32f3
PuLLi <112799107+the-pulli@users.noreply.github.com>
2023-01-24 12:26:37 +0100

    Fix test for non macOS systems

--- test/test_pullbox.rb
+++ test/test_pullbox.rb
@@ -9,6 +9,15 @@ class TestPullbox < Minitest::Test
   end
 
   def test_that_it_can_display_a_dialog
-    Pullbox::AppleScript.display_dialog("Hello", "MailMate", buttons: %w[Cancel OK])
+    mac_system { Pullbox::AppleScript.display_dialog("Hello", "MailMate", buttons: %w[Cancel OK]) }
+  end
+
+  private
+
+  def mac_system
+    return unless block_given?
+
+    osascript = `which osascript`
+    yield if osascript.length.positive?
   end
 end
